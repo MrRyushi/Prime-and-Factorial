ae1180abdc1b9e227380394742cd15fb9731d5ce
MrRyushi <jptmarcellana@gmail.com>
2024-04-20 20:34:10 +0800

    done factorial and prime

--- src/PrimeFactorial.java
+++ src/PrimeFactorial.java
@@ -2,6 +2,7 @@ public class PrimeFactorial{
     public static void main(String[] args) {
         // create an instance of the gui
         GUI gui = new GUI();
+
         // create instance of the controller with the gui as parameter
         Controller controller = new Controller(gui);
     }
